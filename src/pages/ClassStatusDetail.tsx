8bb3c51c3719919dc63aee4fc1393a015c802d5f
HlyanHtetAung <hlyanshapes1999@gmail.com>
2023-03-31 12:17:53 +0630

    new commit

--- src/pages/ClassStatusDetail.tsx
+++ src/pages/ClassStatusDetail.tsx
@@ -1,13 +1,11 @@
-import React, { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import { STYLES } from '../styles';
 
 const ClassStatusDetail = () => {
   const { packageId, trainerId } = useParams();
-  const ary1 = [
-    { date: '20203', classes: [{}] },
-    { date: '20203', classes: [{}] },
-  ];
-
+  const [classStatus, setClassStauts] = useState([]);
+  console.log(classStatus);
   useEffect(() => {
     fetch(
       `${
@@ -15,10 +13,31 @@ const ClassStatusDetail = () => {
       }/classes/get/selected-class?package_id=${packageId}&trainer_id=${trainerId}`
     )
       .then((res) => res.json())
-      .then((data) => console.log(data));
+      .then((data) => setClassStauts(data.data));
   }, []);
 
-  return <div>ClassStatusDetail</div>;
+  return (
+    <div
+      className={`${STYLES.max_width} ${STYLES.margin_center} ${STYLES.paddingX}`}
+    >
+      {classStatus?.map((clsStatusDetail) => (
+        <div className="bg-gray-200 rounded-md p-[10px] drop-shadow-lg my-[20px]">
+          <h2>{clsStatusDetail['date day']}</h2>
+          <div className="flex justify-between  rounded-md cursor-pointer">
+            <h2 className="text-[20px] font-bold">John Cena</h2>
+            <div className="flex items-center gap-[5px] rounded-md">
+              <p className="text-[14px] font-normal bg-primary py-[3px] px-[10px] rounded-full">
+                Yoga
+              </p>
+              <p className="text-[14px] font-normal bg-gray-700 py-[3px] px-[10px] rounded-full text-white">
+                Individual
+              </p>
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default ClassStatusDetail;
